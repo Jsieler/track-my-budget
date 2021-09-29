acb80ae695bec70d760581e84df935786241924a
Joshua Sieler <80868375+Jsieler@users.noreply.github.com>
2021-09-28 18:32:06 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
-
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -26,7 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true
+});
 
 // routes
 app.use(require("./routes/api.js"));
